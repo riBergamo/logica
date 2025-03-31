c7872fb48b774886e15444630f30235c8344e866
Riane Bergamo <rrianebergamo@gmail.com>
2025-03-31 13:32:27 -0300

    update name first class

--- estudos/src/Primeiro.java
+++ estudos/src/Primeiro.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Primeiro {
     public static void main(String[] args) {
         //1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 
